694af3c3a37842ee197f38428bdc39530fc23b9f
TheFileExplorer <126206720+TheFileExplorer@users.noreply.github.com>
2024-05-28 11:55:15 +1000

    Add platforms

--- My project/Assets/Scripts/CameraController.cs
+++ My project/Assets/Scripts/CameraController.cs
@@ -8,9 +8,11 @@ public class Cameracontroller : MonoBehaviour
    public Vector3 posOffset;
    public float smooth;
 
+   Vector3 velocity;
+
    private void LateUpdate()
    {
-    transform.position = Vector3.Lerp(transform.position,target.position+posOffset,smooth*Time.deltaTime);
+    transform.position = Vector3.SmoothDamp(transform.position, target.position + posOffset, ref velocity, smooth);
    }
 
 
