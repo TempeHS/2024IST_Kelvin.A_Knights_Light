cadcdf192b286fa0bfb1089fef32f0fc59274efb
TheFileExplorer <126206720+TheFileExplorer@users.noreply.github.com>
2024-08-06 11:55:16 +1000

    Add in lighting

--- My project/Assets/Scripts/MonsterStomp.cs
+++ My project/Assets/Scripts/MonsterStomp.cs
@@ -8,6 +8,7 @@ public class MonsterStomp : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        
         if(collision.gameObject.tag == "Weak Point")
         {
             Destroy(collision.gameObject);
